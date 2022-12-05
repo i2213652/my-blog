a6c14be49df4034a4ad9394f6c04f0c96194c659
Richter Rojas <richter9721@outlook.com>
2022-12-04 21:08:22 -0500

    actualización footer

--- src/views/components/footer.vue
+++ src/views/components/footer.vue
@@ -1,9 +1,7 @@
 <template>
 	<div>
 		<footer class="footer bg-black small text-center text-white-50">
-			<div class="container px-4 px-lg-5">
-				Copyright &copy; Your Website 2022
-			</div>
+			<div class="container px-4 px-lg-5">Copyright &copy; My Blog 2022</div>
 		</footer>
 	</div>
 </template>
